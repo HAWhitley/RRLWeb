76055b5bffded0d9940df35b49d6e73dad733de5
HAWhitley <HAWhitley@csustudent.net>
2020-10-20 16:00:23 -0400

    Adding pages for basic functionality

---
+++ Website/clientcal.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Client Calendar</title>
+</head>
+<body>
+    <h2>This is the Client Calendar<h2>
+    <a href="clientindex.php">Home</a>
+    <a href="clientcal.php">Schedule an Appointment</a>
+    <a href="index.php">Log Out</a>
+</body>
+</html>
--- Website/clientindex.php
+++ Website/clientindex.php
@@ -7,5 +7,17 @@
 </head>
 <body>
     <h2>This is the Client Home Page</H2>
+    <h2>Rae's Riding Lessons</h2>
+    <a href="clientindex.php">Home</a>
+    <a href="clientcal.php">Schedule an Appointment</a>
+    <a href="index.php">Log Out</a>
+    <center>
+        <h4>About</h4>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <br>
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco <br>
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in <br>
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat <br>
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
+    </center>
 </body>
 </html>
