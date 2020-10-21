25755ca0b596b9e7f076c6cc7a62bef8f6bb272b
HAWhitley <HAWhitley@csustudent.net>
2020-10-21 00:39:50 -0400

    Calendar

--- Website/clientcal.php
+++ Website/clientcal.php
@@ -9,10 +9,11 @@
     <h2>This is the Client Calendar<h2>
     <a href="clientindex.php">Home</a>
     <a href="clientcal.php">Schedule an Appointment</a>
-    <a href="index.php">Log Out</a>
+    <a href="index.php">Log Out</a><br>
     <?php
         session_start();
         echo $_SESSION["user"];
     ?>
+    <iframe src="https://www.picktime.com/raesridinglessons" width="100%" height="1300"></iframe>
 </body>
 </html>
